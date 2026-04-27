8a1debed0b6cb45eef70feecd99e45b8f7b8db68
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-04-27 12:49:06 +0200

    Only initialise once

--- binding.cc
+++ binding.cc
@@ -1,15 +1,25 @@
 #include <assert.h>
 #include <bare.h>
 #include <js.h>
+#include <uv.h>
 
 #include <LibreOfficeKit/LibreOfficeKit.h>
 
 extern "C" LibreOfficeKit *
 libreofficekit_hook(const char *install_path);
 
+static uv_once_t bare_collabora__init_guard = UV_ONCE_INIT;
+
+static LibreOfficeKit *bare_collabora__kit = nullptr;
+
+static void
+bare_collabora__on_init(void) {
+  bare_collabora__kit = libreofficekit_hook(nullptr);
+}
+
 static js_value_t *
 bare_collabora_exports(js_env_t *env, js_value_t *exports) {
-  libreofficekit_hook(nullptr);
+  uv_once(&bare_collabora__init_guard, bare_collabora__on_init);
 
   return exports;
 }
