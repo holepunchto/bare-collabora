0e014313a9a72a9150bcdf438b4527ba80fb080e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-04-27 15:29:50 +0200

    Add types

---
+++ lib/document.d.ts
@@ -0,0 +1,7 @@
+export default interface CollaboraDocument {
+  saveAs(url: string, format?: string, options?: string): boolean
+}
+
+export default class CollaboraDocument {
+  constructor(url: string)
+}
